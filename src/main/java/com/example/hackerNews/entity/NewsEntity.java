1858c3119cdae074de81a475dbd082c74512e2c9
Ankit Srivastava <ankit.srivastava.17.1@mountblue.io>
2021-10-18 15:02:51 +0530

    NewsEntity class created

---
+++ src/main/java/com/example/hackerNews/entity/NewsEntity.java
@@ -0,0 +1,66 @@
+package com.example.hackerNews.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "News")
+public class NewsEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String url;
+    @Column(columnDefinition = "text")
+    private String content;
+    private String createdAt;
+
+    public NewsEntity() {
+    }
+
+    public NewsEntity(String title, String url, String content, String createdAt) {
+        this.title = title;
+        this.url = url;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}
